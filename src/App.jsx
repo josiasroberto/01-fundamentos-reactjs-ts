fcbaad77ee79add0b214cd500cbb3338af6d7ebf
Josias Roberto <josiasroberto1@gmail.com>
2023-06-27 17:40:31 -0300

    First Commit, fundamentos do ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from "./Post"
+
+export function App() {
+    return (
+    <>
+    <Post 
+      author="Josias Roberto" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At debitis molestias, obcaecati accusamus quibusdam dignissimos maiores, placeat suscipit vero voluptate omnis repellendus ipsa deleniti eaque dolor reprehenderit! Rerum, assumenda. Nulla."
+    />
+    
+    <Post 
+      author="Elvis Presley" 
+      content="Reach out to Jesus!"
+    />
+    
+    </>
+  )
+}
+

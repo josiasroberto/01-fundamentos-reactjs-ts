1e7d3220b4e2817913e620825ddfe5375af1722e
Josias Roberto <josiasroberto1@gmail.com>
2023-06-29 19:27:38 -0300

    Ignite Feed project started, components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { Post } from "./Post"
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
+
+import styles from './App.module.css'
+
+import './global.css'
 
 export function App() {
     return (
     <>
-    <Post 
-      author="Josias Roberto" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At debitis molestias, obcaecati accusamus quibusdam dignissimos maiores, placeat suscipit vero voluptate omnis repellendus ipsa deleniti eaque dolor reprehenderit! Rerum, assumenda. Nulla."
-    />
-    
-    <Post 
-      author="Elvis Presley" 
-      content="Reach out to Jesus!"
-    />
+    <Header/>
+    <div className={styles.wrapper}>
+
+      <Sidebar/>
+
+      <main>
+        <Post 
+          author="Josias Roberto" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At debitis molestias, obcaecati accusamus quibusdam dignissimos maiores, placeat suscipit vero voluptate omnis repellendus ipsa deleniti eaque dolor reprehenderit! Rerum, assumenda. Nulla."
+        />
+      
+        <Post 
+          author="Elvis Presley" 
+          content="Reach out to Jesus!"
+        />
+      </main>
+
+    </div>
     
     </>
   )

ad609e62727974a925067eafdc590b910632e1c3
Josias Roberto <josiasroberto1@gmail.com>
2023-07-01 13:43:39 -0300

    creating Like funcionality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,21 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Comment({content ,onDeleteComment}){
+  const [likeCount, setLikeCount] = useState(0)
+  
   function handleDeleteComment(){
     onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    //Sempre que for atualizar uma informação que depende de seu valor anterior
+    //é interessante utilizar este padrão de função
+    setLikeCount((state =>{
+      return state + 1
+    }))
   }
   return(
     <div className={styles.comment}>
@@ -26,10 +37,10 @@ export function Comment({content ,onDeleteComment}){
           <p>{content}</p>
         </div>
 
-        <footer>
+        <footer onClick={handleLikeComment}>
           <button>
             <ThumbsUp/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>

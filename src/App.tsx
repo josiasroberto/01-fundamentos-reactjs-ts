9d58352bccde2a2404281620e9c8d915fa5428f5
Josias Roberto <josiasroberto1@gmail.com>
2023-09-02 16:33:40 -0300

    Add Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
-import { Post } from "./components/Post"
+import { Post ,PostType } from "./components/Post"
+
 
 import styles from './App.module.css'
 
 import './global.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author:{
@@ -15,9 +16,9 @@ const posts = [
       role: 'Piloto'
     },
     content: [
-      {type: 'paragraph', content: 'Fala galeraa 👋'},
-      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      {type: 'link', content: 'jane.design/doctorcare'},      
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é Ignite Feed 🚀'},
+      {type: 'link', content: 'josias.design/ignitefeed'},      
     ],
     publishedAt: new Date('2023-06-30 10:00:00'),
   },
@@ -29,9 +30,9 @@ const posts = [
       role: 'Web developer'
     },
     content: [
-      {type: 'paragraph', content: 'Fala galeraa 👋'},
-      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      {type: 'link', content: 'jane.design/doctorcare'},      
+      {type: 'paragraph', content: 'Fala pessoal 👋'},
+      {type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui. Acesse e deixe seu feedback'},
+      {type: 'link', content: 'jakeliny.design'},      
     ],
     publishedAt: new Date('2023-06-28 10:00:00'),
   },
@@ -50,9 +51,7 @@ export function App() {
           return (
             <Post 
               key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
+              post={post}
             />
           )
         })}
